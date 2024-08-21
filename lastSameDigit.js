d292a0e3a1793e567b60e60ba70a4b26c7b1cca7
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-21 19:42:20 +0500

    1

---
+++ lastSameDigit.js
@@ -0,0 +1,12 @@
+let num1=1234;
+let num2=435;
+
+let s1=num1.toString();
+let s2=num2.toString();
+
+if(s1[s1.length-1]===s2[s2.length-1]){
+    console.log("are same");
+}
+else{
+    console.log("not same");
+};
