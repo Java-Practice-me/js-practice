bad2897c6b8ce8701f11ca3ce0ceaeaf23e834f7
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-08-21 19:40:38 +0500

    8

---
+++ function.js
@@ -0,0 +1,26 @@
+// let obj={
+//     name:"virus",
+//     userName:"shizuka-virus",
+// }
+
+// function objHandle(aiman){
+//     return aiman.name;
+// }
+
+// let nm=objHandle(obj);
+
+// console.log(nm);
+
+// let arr=[1,2,3,4,5,6];
+
+// function arrPrint(virus){
+//     console.log(virus[0]);
+// }
+// arrPrint(arr);
+
+function integersVal(...arr){
+    return arr;
+}
+let brr=integersVal(2,3,4,5,6,7,8,9);
+
+console.log(brr);
